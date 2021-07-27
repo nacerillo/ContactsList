2d446080d76319f6846c9ca5552d7936d32de061
nacerillo <nicholascerillo@gmail.com>
2021-07-27 15:41:58 -0400

    adding home and about page

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p className = "my-1">
+                This is a contacts list app!
+            </p>
+            <p className = "bg-dark p">
+                <strong>version 1.0.0</strong>
+            </p>
+        </div>
+    )
+}
+export default About
