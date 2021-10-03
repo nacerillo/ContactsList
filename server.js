bb847c8e6b407e7d8da0ce39b147d08c64c988b0
nacerillo <nicholascerillo@gmail.com>
2021-10-03 14:54:27 -0400

    updating package and server for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { oneOf } = require("express-validator");
 const connectDB = require("./config/db"); 
+const path = require('path');
 //defining our routes
 const app = express();
 
@@ -13,9 +14,17 @@ app.use(express.json({extended: false}));
 app.get('/', (req,res) => res.json({msg : `Welcome to the Rice Fields MotherLovers!`})
 );
 
+//Application Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+//Serve static assets in productions
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server is up and running on port ${PORT}`));
